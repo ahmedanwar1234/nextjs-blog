433c07f1121d9342e9e718a797efee222c882e98
ahmed anwar <143233702+ahmedanwar1234@users.noreply.github.com>
2024-03-27 06:04:26 +0200

    update fucker

--- pages/about/index.js
+++ pages/about/index.js
@@ -125,7 +125,7 @@ const About = () => {
             exit="hidden"
             className="max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0"
           >
-            10 years ago, I began freelancing as a developer. Since then, I have
+            fucker 10 years ago, I began freelancing as a developer. Since then, I have
             done remte work for agencies, counsulted for startups, and
             collaboreted on digital products for business and consumer use
           </motion.p>
